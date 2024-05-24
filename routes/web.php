43834864cca6dbbd7797b8d6baa1770b98d23e3e
nhieltanedo <tanedonhielpatrick@gmail.com>
2024-05-24 18:38:42 +0800

    breeze success installed

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +17,29 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [PostController::class, 'postIndex'])->name('post.postIndex');
+
+Route::middleware('auth', 'verified')->group(function () {
+    // Route::get('/dashboard', function () {
+    //     return view('dashboard');
+    // })->name('dashboard');
+
+    Route::get('/dashboard', [DashboardController::class, 'Dashboard'])->name('dashboard');
+
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Route::get('/post', [PostController::class, 'index'])->name('post.index');
+    // Route::get('/post/create', [PostController::class, 'create'])->name('post.create');
+    // Route::post('/post/create', [PostController::class, 'store'])->name('post.store');
+    // Route::get('/post/{post}/edit', [PostController::class, 'edit'])->name('post.edit');
+    // Route::put('/post/{post}', [PostController::class, 'update'])->name('post.update');
+    // Route::get('/post/{post}', [PostController::class, 'show'])->name('post.show');
+    // Route::delete('/post/{post}', [PostController::class, 'destroy'])->name('post.destroy');
+
+    Route::resource('post', PostController::class);
+    Route::get('/allposts', [PostController::class, 'allposts'])->name('post.allposts');
 });
+
+require __DIR__ . '/auth.php';
